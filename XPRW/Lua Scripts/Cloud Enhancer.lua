528bd3344c3e57c3744c4dca97a14d8136710329
GearUp <76500443+GearUpsim@users.noreply.github.com>
2020-12-23 20:37:14 +0100

    Create Cloud Enhancer.lua

---
+++ XPRW/Lua Scripts/Cloud Enhancer.lua
@@ -0,0 +1,33 @@
+-- Old Version, Placeholder to make PRs easier
+
+set( "sim/private/controls/clouds/first_res_3d", 3)
+set( "sim/private/controls/clouds/last_res_3d", 5)
+set( "sim/private/controls/clouds/plot_radius",  1.1)
+set( "sim/private/controls/clouds/limit_far",  0.01)
+set( "sim/private/controls/clouds/soft_occlude",  1.0)
+set( "sim/private/controls/clouds/overdraw_control", 0.3)
+
+set( "sim/private/controls/clouds/ambient_gain", 1.3)
+set( "sim/private/controls/clouds/diffuse_gain", 1.05)
+set( "sim/private/controls/hdr/white_point", 4.2)
+set( "sim/private/controls/clouds/check_z_hdr", 1)
+
+set( "sim/private/controls/clouds/light_curve_power", 1)
+set( "sim/private/controls/clouds/light_curve_ratio", 1)
+set( "sim/private/controls/clouds/spec_gain", 0.9)
+
+set( "sim/private/controls/cloud/fade_near_start", 0.1)
+set( "sim/private/controls/cloud/fade_far_start", 1.0)
+set( "sim/private/controls/cloud/fade_far_end", 5.2)
+set( "sim/private/controls/cloud/fade_near_end", 10.5)
+
+set( "sim/private/controls/clouds/image_size_x", 4096.0)
+set( "sim/private/controls/clouds/image_size_y", 4096.0)
+set( "sim/private/controls/clouds/shadow_size", 4096.0 )
+
+set("sim/private/controls/clouds/shad_radius", 0.4)
+set("sim/private/controls/clouds/cloud_shadow_lighten_ratio", 0.99)
+set("sim/private/controls/clouds/shad_alpha_dry", 0.55)
+set("sim/private/controls/clouds/shad_alpha_wet", 0.95)
+
+set( "sim/private/controls/skyc/white_out_in_clouds", 0.0)
